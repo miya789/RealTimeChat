fd10fb817e40aa3c759c709b91d2f37211507fff
root <root@ip-10-0-0-10.ap-northeast-1.compute.internal>
2019-09-06 00:58:00 +0000

    Add swithing by channel_id

--- server.js
+++ server.js
@@ -154,6 +154,8 @@ app.post('/sendMessage', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (msg) => {
+        console.log(typeof(msg));
+        console.log(msg);
         io.emit('chat message', msg);
         console.log(`message: ${msg}`);
     });
