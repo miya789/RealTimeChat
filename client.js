59379698c581b775fb097111b9f061ef9c50ed21
Glitch (curious-caravan) <none>
2019-12-10 14:13:05 +0000

    🎖😂 Updated with Glitch about package.json

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
-const io = require('socket.io-client');
-const socket = io('http://13.113.66.127:3000/');
+// const io = require('socket.io-client');
+// const socket = io('http://13.113.66.127:3000/');
 
-socket.on('connect', () => {
-    console.log("connected!")
-    socket.emit('chat message', 'send message.');
-    socket.disconnect()
-    // socket.on('chat message', (msg) => {
-    //     // io.emit('chat message', msg);
-    // });
-});
+// socket.on('connect', () => {
+//     console.log("connected!")
+//     socket.emit('chat message', 'send message.');
+//     socket.disconnect()
+//     // socket.on('chat message', (msg) => {
+//     //     // io.emit('chat message', msg);
+//     // });
+// });
